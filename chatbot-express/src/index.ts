540d11182a62d8a3dd644de6ebda58d1dde3d797
aryanc381 <chauhanaryan381@gmail.com>
2026-09-19 12:40:02 +0530

    feat: p1-chatbot is done.

--- chatbot-express/src/index.ts
+++ chatbot-express/src/index.ts
@@ -3,7 +3,8 @@ import rootRouter from './routes/index.js';
 import { env } from './env.js';
 
 const app = express();
+app.use(express.json());
 
 app.use('/api/v1', rootRouter);
 
-app.listen(env.PORT, () => { console.log(`App is listening at PORT-${env.PORT}`)});
+app.listen(3500, () => { console.log(`App is listening at PORT-${3500}`)});
--- chatbot-express/src/routes/chat.route.ts
+++ chatbot-express/src/routes/chat.route.ts
@@ -7,14 +7,17 @@ const router = express.Router();
 router.post('/', async(req, res) => {
     const request = req.body;
     const openai = new OpenAI({
-        apiKey: env.CLOUDFLARE_API_TOKEN,
-	    baseURL: `https://api.cloudflare.com/client/v4/accounts/${env.CLOUDFLARE_ACCESS_KEY_ID}/ai/v1`,
+        apiKey: `${env.CLOUDFLARE_API_TOKEN}`,
+	    baseURL: `https://api.cloudflare.com/client/v4/accounts/${env.CLOUDFLARE_ACCOUNT_ID}/ai/v1`,
     });
 
     const chatCompletion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: "Make some robot noises" }],
-        model: "@cf/meta/llama-3.1-8b-instruct"
+        messages: [{ role: "user", content: request.question }],
+        model: "@cf/ibm-granite/granite-4.0-h-micro"
     });
+
+    const answer = chatCompletion.choices[0]?.message?.content;
+    return res.json({ status: 200, msg: answer });
 });
 
 
